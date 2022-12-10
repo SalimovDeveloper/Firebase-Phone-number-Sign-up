c69d1f91f744977d0d9248972f51d52663f0845e
salimovdeveloper <salimovdeveloper@gmail.com>
2022-12-10 09:43:34 -0600

    commit 2

--- app/src/main/java/uz/salimovdeveloper/firebasephonenumbersignup/MainActivity.kt
+++ app/src/main/java/uz/salimovdeveloper/firebasephonenumbersignup/MainActivity.kt
@@ -2,10 +2,23 @@ package uz.salimovdeveloper.firebasephonenumbersignup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthProvider
+import uz.salimovdeveloper.firebasephonenumbersignup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+    private val TAG = "MainActivity"
+    lateinit var storedVerificationId:String
+    lateinit var resendToken:PhoneAuthProvider.ForceResendingToken
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+        auth.setLanguageCode("uz")
+
     }
 }
